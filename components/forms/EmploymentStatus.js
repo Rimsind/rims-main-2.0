cf3ba414b0aba1c51a6dcfd6eefa1a5505fbf7ed
Rabindranath Mandal <rimstech21@gmail.com>
2022-02-02 18:17:59 +0530

    Medical Info Update

--- components/forms/EmploymentStatus.js
+++ components/forms/EmploymentStatus.js
@@ -24,233 +24,204 @@ const EmploymentStatus = () => {
 
   return (
     <>
-      <div className="accordion-item border rounded mt-2">
-        <h2 className="accordion-header" id="headingFour">
-          <button
-            className="accordion-button border-0 bg-primary collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseFour"
-            aria-expanded="false"
-            aria-controls="collapseFour"
-            style={{ color: "aliceblue" }}
-          >
-            <div className="row align-items-center">
-              <div className="col-md-12">Employment Status</div>
-            </div>
-          </button>
-        </h2>
+      <form>
         <div
-          id="collapseFour"
-          className="accordion-collapse border-0 collapse"
-          aria-labelledby="headingFour"
-          data-bs-parent="#accordionExample"
+          className="gen-form mb-3"
+          style={{ borderBottom: "1px solid #bbbaba" }}
         >
-          <div className="accordion-body text-muted">
-            <form>
-              <div
-                className="gen-form mb-3"
-                style={{ borderBottom: "1px solid #bbbaba" }}
-              >
-                <div className="row justify-content-between align-items-start">
-                  <div className="col-md-3">
-                    <h3 className="fs-6 fs-bold text-dark">Work Status</h3>
+          <div className="row justify-content-between align-items-start">
+            <div className="col-md-3">
+              <h3 className="fs-6 fs-bold text-dark">Work Status</h3>
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Work Full Time"
+                      />
+                    </div>
+                    <div className="col-md-9">
+                      <p className="space-x-4">Work Full Time</p>
+                    </div>
                   </div>
-                  <div className="col-md-9">
-                    <div className="row">
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Work Full Time"
-                            />
-                          </div>
-                          <div className="col-md-9">
-                            <p className="space-x-4">Work Full Time</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Work Part Time"
-                            />
-                          </div>
-                          <div className="col-md-10">
-                            <p className="space-x-4">Work Part Time</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Work Light Duty"
-                            />
-                          </div>
-                          <div className="col-md-9">
-                            <p className="space-x-4">Work Light Duty</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Homemaker"
-                            />
-                          </div>
-                          <div className="col-md-10">
-                            <p className="space-x-4">Homemaker</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Retired"
-                            />
-                          </div>
-                          <div className="col-md-10">
-                            <p className="space-x-4">Retired</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Disabled"
-                            />
-                          </div>
-                          <div className="col-md-10">
-                            <p className="space-x-4">Disabled</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Unemployed"
-                            />
-                          </div>
-                          <div className="col-md-8">
-                            <p className="space-x-4">Unemployed</p>
-                          </div>
-                        </div>
-                      </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Work Part Time"
+                      />
+                    </div>
+                    <div className="col-md-10">
+                      <p className="space-x-4">Work Part Time</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Work Light Duty"
+                      />
+                    </div>
+                    <div className="col-md-9">
+                      <p className="space-x-4">Work Light Duty</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Homemaker"
+                      />
+                    </div>
+                    <div className="col-md-10">
+                      <p className="space-x-4">Homemaker</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Retired"
+                      />
+                    </div>
+                    <div className="col-md-10">
+                      <p className="space-x-4">Retired</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Disabled"
+                      />
+                    </div>
+                    <div className="col-md-10">
+                      <p className="space-x-4">Disabled</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Unemployed"
+                      />
+                    </div>
+                    <div className="col-md-8">
+                      <p className="space-x-4">Unemployed</p>
+                    </div>
+                  </div>
+                </div>
 
-                      <div className="col-md-4">
-                        <div className="row">
-                          <div className="col-md-2">
-                            <input
-                              className="form-check-input"
-                              type="radio"
-                              name="workStatus"
-                              value="Student"
-                            />
-                          </div>
-                          <div className="col-md-8">
-                            <p className="space-x-4">Student</p>
-                          </div>
-                        </div>
-                      </div>
+                <div className="col-md-4">
+                  <div className="row">
+                    <div className="col-md-2">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="workStatus"
+                        value="Student"
+                      />
+                    </div>
+                    <div className="col-md-8">
+                      <p className="space-x-4">Student</p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div
-                className="gen-form mb-3"
-                style={{ borderBottom: "1px solid #bbbaba" }}
-              >
-                <div className="row justify-content-between align-items-center mb-3">
-                  <div className="col-md-3">
-                    <h3 className="fs-6 fs-bold text-dark">Occupation?</h3>
-                  </div>
-                  <div className="col-md-9">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="occupation"
-                      placeholder=""
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="gen-form mb-3">
-                <div className="row justify-centent-between align-items-start">
-                  <div className="col-md-3">
-                    <h3 className="fs-6 fs-bold text-dark">
-                      Your Work Involves (Check all that apply)
-                    </h3>
-                  </div>
-                  <div className="col-md-9">
-                    <div className="row">
-                      {work_involves.map((item, index) => (
-                        <div className="col-md-4" key={index}>
-                          <div className="row">
-                            <div className="col-md-2">
-                              <input
-                                className="form-check-input"
-                                type="checkbox"
-                                name="workInvolves"
-                                value={item}
-                              />
-                            </div>
-                            <div className="col-md-10">
-                              <p className="space-x-4">{item}</p>
-                            </div>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="gen-form-soft-button">
-                <div className="row">
-                  <div className="col-md-4"></div>
-                  <div className="col-md-4"></div>
-                  <div className="col-md-4">
-                    <div
-                      className="right-button"
-                      style={{ textAlign: "right" }}
-                    >
-                      <button type="submit" className="btn btn-success">
-                        Save Changes
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
+        <div
+          className="gen-form mb-3"
+          style={{ borderBottom: "1px solid #bbbaba" }}
+        >
+          <div className="row justify-content-between align-items-center mb-3">
+            <div className="col-md-3">
+              <h3 className="fs-6 fs-bold text-dark">Occupation?</h3>
+            </div>
+            <div className="col-md-9">
+              <input
+                type="text"
+                className="form-control"
+                name="occupation"
+                placeholder=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="gen-form mb-3">
+          <div className="row justify-centent-between align-items-start">
+            <div className="col-md-3">
+              <h3 className="fs-6 fs-bold text-dark">
+                Your Work Involves (Check all that apply)
+              </h3>
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                {work_involves.map((item, index) => (
+                  <div className="col-md-4" key={index}>
+                    <div className="row">
+                      <div className="col-md-2">
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          name="workInvolves"
+                          value={item}
+                        />
+                      </div>
+                      <div className="col-md-10">
+                        <p className="space-x-4">{item}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="gen-form-soft-button">
+          <div className="row">
+            <div className="col-md-4"></div>
+            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <div className="right-button" style={{ textAlign: "right" }}>
+                <button type="submit" className="btn btn-success">
+                  Save Changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </>
   );
 };
--- pages/user/medical-information.js
+++ pages/user/medical-information.js
@@ -24,15 +24,123 @@ const MedicalInformation = () => {
               <UserNav status5="active" />
 
               <div className="col-md-7 col-lg-8 col-xl-9">
-                <div className="card">
+                <div className="card shadow-sm">
                   <div className="card-body">
-                    <GeneralInformation />
-                    <SocialHistory />
-                    <EmploymentStatus />
-                    <MedicalHistory />
-                    <FunctionalStatus />
-                    <FamilyMadicalHistory />
-                    <UploadMedicalRecord />
+                    <div class="flex-fill">
+                      <div class="card-header">
+                        <ul
+                          role="tablist"
+                          class="nav nav-tabs card-header-tabs"
+                        >
+                          <li class="nav-item">
+                            <a
+                              href="#tab-1"
+                              data-bs-toggle="tab"
+                              class="nav-link active"
+                            >
+                              Genral Information
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a
+                              href="#tab-2"
+                              data-bs-toggle="tab"
+                              class="nav-link"
+                            >
+                              Social Histroy & Living Environment
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a
+                              href="#tab-3"
+                              data-bs-toggle="tab"
+                              class="nav-link"
+                            >
+                              Employment Status
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a
+                              href="#tab-4"
+                              data-bs-toggle="tab"
+                              class="nav-link"
+                            >
+                              Medical History
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a
+                              href="#tab-5"
+                              data-bs-toggle="tab"
+                              class="nav-link"
+                            >
+                              Functional Status
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a
+                              href="#tab-6"
+                              data-bs-toggle="tab"
+                              class="nav-link"
+                            >
+                              Family Medical History
+                            </a>
+                          </li>
+                          <li class="nav-item">
+                            <a
+                              href="#tab-7"
+                              data-bs-toggle="tab"
+                              class="nav-link"
+                            >
+                              Medical Records
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                      <div class="card-body">
+                        <div class="tab-content pt-0">
+                          <div
+                            role="tabpanel"
+                            id="tab-1"
+                            class="tab-pane fade show active"
+                          >
+                            <div class="row">
+                              <GeneralInformation />
+                            </div>
+                          </div>
+                          <div role="tabpanel" id="tab-2" class="tab-pane fade">
+                            <div class="row">
+                              <SocialHistory />
+                            </div>
+                          </div>
+                          <div role="tabpanel" id="tab-3" class="tab-pane fade">
+                            <div class="row">
+                              <EmploymentStatus />
+                            </div>
+                          </div>
+                          <div role="tabpanel" id="tab-4" class="tab-pane fade">
+                            <div class="row">
+                              <MedicalHistory />
+                            </div>
+                          </div>
+                          <div role="tabpanel" id="tab-5" class="tab-pane fade">
+                            <div class="row">
+                              <FunctionalStatus />
+                            </div>
+                          </div>
+                          <div role="tabpanel" id="tab-6" class="tab-pane fade">
+                            <div class="row">
+                              <FamilyMadicalHistory />
+                            </div>
+                          </div>
+                          <div role="tabpanel" id="tab-7" class="tab-pane fade">
+                            <div class="row">
+                              <UploadMedicalRecord />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
