a03927226a6f96be3f35c1a4cb2b2a0bea2894f8
Rabindranath Mandal <rimstech21@gmail.com>
2022-02-09 18:28:57 +0530

    Schedule Update

--- pages/schedule.js
+++ pages/schedule.js
@@ -103,82 +103,253 @@ const Schedule = () => {
                   <div className="schedule-cont">
                     <div className="row">
                       <div className="col-md-12">
-                        <div className="time-slot">
-                          <ul className="clearfix">
-                            <li>
+                        <div className="time-slot row mb-2">
+                          <div className="col-2 col-md-2  col-xl-2 col-lg-2 mb-2">
+                            <div className="schd-item me-2">
                               <button
-                                className="btm btn-primary my-2 mx-2"
-                                value="9:00 AM"
-                                onClick={(e) => setData(e.target.value)}
-                              >
-                                9:00 AM
-                              </button>
-
-                              <button
-                                className="btm btn-primary my-2"
-                                value="10:00 AM"
-                                onClick={(e) => setData(e.target.value)}
-                              >
-                                9:00 AM
-                              </button>
-                              <button
-                                className="btm btn-primary my-2"
-                                value="9:00 AM"
-                                onClick={(e) => setData(e.target.value)}
-                              >
-                                9:00 AM
-                              </button>
-                            </li>
-                            <li>
-                              <button
-                                className="btm btn-primary my-2"
+                                className="btn btn-primary rounded-1"
                                 value="9:00 AM"
                                 onClick={(e) => setData(e.target.value)}
                               >
                                 9:00 AM - 9:15 AM
                               </button>
-
+                            </div>
+                          </div>
+                          <div className="col-2 col-md-2  col-xl-2 col-lg-2 mb-2">
+                            <div className="schd-item me-2">
                               <button
-                                className="btm btn-primary my-2"
+                                className="btn btn-primary rounded-1"
                                 value="9:00 AM"
                                 onClick={(e) => setData(e.target.value)}
                               >
-                                9:00 AM
+                                9:00 AM - 9:15 AM
                               </button>
+                            </div>
+                          </div>
+                          <div className="col-2 col-md-2  col-xl-2 col-lg-2 mb-2">
+                            <div className="schd-item me-2">
                               <button
-                                className="btm btn-primary my-2"
+                                className="btn btn-primary rounded-1"
                                 value="9:00 AM"
                                 onClick={(e) => setData(e.target.value)}
                               >
-                                9:00 AM
+                                9:00 AM - 9:15 AM
                               </button>
-                            </li>
-                            <li>
+                            </div>
+                          </div>
+                          <div className="col-2 col-md-2  col-xl-2 col-lg-2 mb-2">
+                            <div className="schd-item me-2">
                               <button
-                                className="btm btn-primary my-2"
+                                className="btn btn-primary rounded-1"
                                 value="9:00 AM"
                                 onClick={(e) => setData(e.target.value)}
                               >
-                                9:00 AM
+                                9:00 AM - 9:15 AM
                               </button>
-
+                            </div>
+                          </div>
+                          <div className="col-2 col-md-2  col-xl-2 col-lg-2 mb-2">
+                            <div className="schd-item me-2">
                               <button
-                                className="btm btn-primary my-2"
+                                className="btn btn-primary rounded-1"
                                 value="9:00 AM"
                                 onClick={(e) => setData(e.target.value)}
                               >
-                                9:00 AM
+                                9:00 AM - 9:15 AM
                               </button>
+                            </div>
+                          </div>
+                          <div className="col-2 col-md-2  col-xl-2 col-lg-2 mb-2">
+                            <div className="schd-item me-2">
                               <button
-                                className="btm btn-primary my-2"
+                                className="btn btn-primary rounded-1"
                                 value="9:00 AM"
                                 onClick={(e) => setData(e.target.value)}
                               >
-                                9:00 AM
+                                9:00 AM - 9:15 AM
                               </button>
-                            </li>
-                          </ul>
+                            </div>
+                          </div>
+                          <div className="col-2 col-md-2  col-xl-2 col-lg-2 mb-2">
+                            <div className="schd-item me-2">
+                              <button
+                                className="btn btn-primary rounded-1"
+                                value="9:00 AM"
+                                onClick={(e) => setData(e.target.value)}
+                              >
+                                9:00 AM - 9:15 AM
+                              </button>
+                            </div>
+                          </div>
                         </div>
+                        {/* <div className="time-slot row mb-2">
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              10:30 AM - 10:45 AM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              10:45 AM - 11:00 AM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              11:00 AM - 11:15 AM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              11:15 AM - 11:30 AM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              11:30 AM - 11:45 AM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              11:45 AM - 12:00 PM
+                            </button>
+                          </div>
+                        </div> */}
+                        {/* <div className="time-slot row mb-2">
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              12:00 PM - 12:15 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              12:15 PM - 12:30 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              12:30 PM - 12:45 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              12:45 PM - 01:00 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              01:00 PM - 01:15 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              01:15 PM - 01:30 PM
+                            </button>
+                          </div>
+                        </div> */}
+                        {/* <div className="time-slot row mb-2">
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              01:30 PM - 01:45 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              01:45 PM - 02:00 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              02:00 PM - 02:15 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              02:15 PM - 02:30 PM
+                            </button>
+                          </div>
+                          <div className="schd-item me-2">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              02:30 PM - 02:45 PM
+                            </button>
+                          </div>
+                          <div className="schd-item">
+                            <button
+                              className="btn btn-primary rounded-1"
+                              value="9:00 AM"
+                              onClick={(e) => setData(e.target.value)}
+                            >
+                              02:45 PM - 03:00 PM
+                            </button>
+                          </div>
+                        </div> */}
                       </div>
                     </div>
                   </div>
