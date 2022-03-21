0e9f518296e8ba7522a141b00d292ae71c9e606d
Rims <83367739+Rimsind@users.noreply.github.com>
2022-03-21 16:03:53 -0700

    dfdf

--- pages/doctors/index.js
+++ pages/doctors/index.js
@@ -41,7 +41,40 @@ const Index = () => {
                       </form>
                     </div>
                   </div>
-                  <LocationFilter />
+                  <div className="card search-filter">
+                    <div className="card-header">
+                      <h4 className="card-title mb-0">Location Filter</h4>
+                    </div>
+                    <div className="card-body">
+                      <div className="filter-widget">
+                        <select
+                          className="form-select"
+                          aria-label="Default select example"
+                        >
+                          <option selected>Select State</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                      <div className="filter-widget">
+                        <select
+                          className="form-select"
+                          aria-label="Default select example"
+                        >
+                          <option selected>Select City</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                      <div className="btn-search">
+                        <button type="button" className="btn w-100">
+                          Search
+                        </button>
+                      </div>
+                    </div>
+                  </div>
 
                   <div className="card search-filter">
                     <div className="card-header">
