a845d0c2441de9ccb1714d245002aa305b2eca66
Rabindranath Mandal <rimstech21@gmail.com>
2022-04-19 12:21:56 +0530

    New minor uodate

--- pages/doctors/index.js
+++ pages/doctors/index.js
@@ -243,7 +243,7 @@ const Index = () => {
                         .slice(`${startValue}`, `${endValue}`)
                         .map((item, index) => (
                           <div
-                            className="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-2 col-xxl-2"
+                            className="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3"
                             key={index}
                           >
                             <div className="profile-widget me-3">
