c182224946722bc1bcf4aef95d92da6e75c9e9bc
Rims <83367739+Rimsind@users.noreply.github.com>
2022-04-20 15:52:07 +0530

    gyfc

--- components/common/DoctorTimetableCard.js
+++ components/common/DoctorTimetableCard.js
@@ -24,7 +24,6 @@ const DoctorTimetableCard = ({ data, doctorId }) => {
             </div>
           </div>
           <div className="col-12 col-sm-12 col-md-12 col-lg-9 col-xl-9 col-xxl-9 py-3">
-<<<<<<< HEAD
             <div className="doctor-time-slot d-flex align-items-center justify-content-between">
               {data?.slots?.map((items, index) => (
                 <div
@@ -52,31 +51,6 @@ const DoctorTimetableCard = ({ data, doctorId }) => {
                       Book Now <sup className="">{items?.seats}</sup>
                     </a>
                   </Link>
-=======
-            <div className="doctor-time-slot row align-items-center justify-content-between">
-              {data?.schedule?.map((items, index) => (
-                <div className="col">
-                  <div
-                    className="doctor-location-items text-center pe-4 border-end border-1"
-                    key={index}
-                  >
-                    <a
-                      className="fs-6 py-1 px-4 shadow-sm text-dark"
-                      style={{ backgroundColor: "#e5e5e569" }}
-                    >
-                      {items?.day}
-                    </a>
-                    <p className="fs-6 text-dark mt-2">{items?.timeSlots}</p>
-                    <Link href="#">
-                      <button
-                        className="btn text-light fs-6 rounded-pill"
-                        style={{ backgroundColor: "#1d00a3" }}
-                      >
-                        Book Now
-                      </button>
-                    </Link>
-                  </div>
->>>>>>> d32c59b243fff7663ab4cb20b317a81ca66b1d90
                 </div>
               ))}
             </div>
