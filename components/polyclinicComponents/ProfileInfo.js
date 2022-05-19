6518f73bc7eace6057efe7ec1da517297928ea66
Rabindranath Mandal <rimsrabindra12@gmail.com>
2022-05-19 13:35:22 +0530

    New Update

--- components/polyclinicComponents/ProfileInfo.js
+++ components/polyclinicComponents/ProfileInfo.js
@@ -190,44 +190,170 @@ const ProfileInfo = ({ data }) => {
                 </div>
               </div>
             </div>
+            <div className="row">
+              <div className="col-12 col-md-12">
+                <div className="form-group">
+                  <label className="mb-2" htmlFor="floatingTextarea">
+                    Overview
+                  </label>
+                  <textarea
+                    className="form-control"
+                    placeholder="Overview"
+                    id="floatingTextarea"
+                    rows="10"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
             <div className="row align-items-center">
+              <p className="workings-hours-title text-center my-5 fs-5 fw-bold border-bottom border-1 pb-3">
+                Working Hours
+              </p>
               <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <div className="row align-items-center">
-                  <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                    <label>Start Time</label>
+                  <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                    <label>Start Time -</label>
                   </div>
-                  <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8 col-xxl-8">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="e.g 09:00 AM"
-                      {...register("start_time")}
-                      defaultValue={
-                        !!data?.workingHours && !!data?.workingHours?.start_time
-                          ? data?.workingHours?.start_time
-                          : ""
-                      }
-                    />
+                  <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                    <select
+                      className="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option>Open 24 Hrs</option>
+                      <option>00:00</option>
+                      <option>00:30</option>
+                      <option>01:00</option>
+                      <option>01:30</option>
+                      <option>02:00</option>
+                      <option>02:30</option>
+                      <option>03:00</option>
+                      <option>03:30</option>
+                      <option>04:00</option>
+                      <option>04:30</option>
+                      <option>05:00</option>
+                      <option>05:30</option>
+                      <option>06:00</option>
+                      <option>06:30</option>
+                      <option>07:00</option>
+                      <option>07:30</option>
+                      <option>08:00</option>
+                      <option>08:30</option>
+                      <option>09:00</option>
+                      <option>09:30</option>
+                      <option>10:00</option>
+                      <option>10:30</option>
+                      <option>11:00</option>
+                      <option>11:30</option>
+                      <option>12:00</option>
+                      <option>12:30</option>
+                      <option>13:00</option>
+                      <option>13:30</option>
+                      <option>14:00</option>
+                      <option>14:30</option>
+                      <option>15:00</option>
+                      <option>15:30</option>
+                      <option>16:00</option>
+                      <option>16:30</option>
+                      <option>17:00</option>
+                      <option>17:30</option>
+                      <option>18:00</option>
+                      <option>18:30</option>
+                      <option>19:00</option>
+                      <option>19:30</option>
+                      <option>20:00</option>
+                      <option>20:30</option>
+                      <option>21:00</option>
+                      <option>21:30</option>
+                      <option>22:00</option>
+                      <option>22:30</option>
+                      <option>23:00</option>
+                      <option>23:30</option>
+                      <option>24:00</option>
+                      <option>24:30</option>
+                    </select>
+                  </div>
+                  <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                    <select
+                      className="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option>AM</option>
+                      <option>PM</option>
+                    </select>
                   </div>
                 </div>
               </div>
               <div className="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <div className="row align-items-center">
-                  <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
-                    <label>End Time</label>
+                  <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4 col-xxl-4 text-center">
+                    <label>End Time -</label>
                   </div>
-                  <div className="col-12 col-sm-12 col-md-6 col-lg-8 col-xl-8 col-xxl-8">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="e.g 09:00 PM"
-                      {...register("end_time")}
-                      defaultValue={
-                        !!data?.workingHours && !!data?.workingHours?.end_time
-                          ? data?.workingHours?.end_time
-                          : ""
-                      }
-                    />
+                  <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                    <select
+                      className="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option>Open 24 Hrs</option>
+                      <option>00:00</option>
+                      <option>00:30</option>
+                      <option>01:00</option>
+                      <option>01:30</option>
+                      <option>02:00</option>
+                      <option>02:30</option>
+                      <option>03:00</option>
+                      <option>03:30</option>
+                      <option>04:00</option>
+                      <option>04:30</option>
+                      <option>05:00</option>
+                      <option>05:30</option>
+                      <option>06:00</option>
+                      <option>06:30</option>
+                      <option>07:00</option>
+                      <option>07:30</option>
+                      <option>08:00</option>
+                      <option>08:30</option>
+                      <option>09:00</option>
+                      <option>09:30</option>
+                      <option>10:00</option>
+                      <option>10:30</option>
+                      <option>11:00</option>
+                      <option>11:30</option>
+                      <option>12:00</option>
+                      <option>12:30</option>
+                      <option>13:00</option>
+                      <option>13:30</option>
+                      <option>14:00</option>
+                      <option>14:30</option>
+                      <option>15:00</option>
+                      <option>15:30</option>
+                      <option>16:00</option>
+                      <option>16:30</option>
+                      <option>17:00</option>
+                      <option>17:30</option>
+                      <option>18:00</option>
+                      <option>18:30</option>
+                      <option>19:00</option>
+                      <option>19:30</option>
+                      <option>20:00</option>
+                      <option>20:30</option>
+                      <option>21:00</option>
+                      <option>21:30</option>
+                      <option>22:00</option>
+                      <option>22:30</option>
+                      <option>23:00</option>
+                      <option>23:30</option>
+                      <option>24:00</option>
+                      <option>24:30</option>
+                    </select>
+                  </div>
+                  <div className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                    <select
+                      className="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option>AM</option>
+                      <option>PM</option>
+                    </select>
                   </div>
                 </div>
               </div>
